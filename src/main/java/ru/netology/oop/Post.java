7a9e136722f308323228222389ea12f093d73784
Роман Шапарь <romanshaparution@gmail.com>
2022-03-03 09:21:59 +0300

    added file

---
+++ src/main/java/ru/netology/oop/Post.java
@@ -0,0 +1,13 @@
+package ru.netology.oop;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    String subscription;
+    public FormDate birthday;
+
+
+}
